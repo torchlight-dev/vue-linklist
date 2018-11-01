022661507f197d51feac98f0aff2e5a5405a37fd
genichiro <kokukyou@gmail.com>
2018-11-01 21:17:02 +0900

    apply changes when source was modified

--- src/linklist.vue
+++ src/linklist.vue
@@ -221,6 +221,30 @@ export default {
     },
     categoryTransform(index) {
       return `translate(${this.categoryX(index)}, 0)`;
+    },
+    refreshPairs() {
+      this.pairs = this.pairs.filter(pair => {
+        const validatedStart = this.source.some(category => {
+          return pair.start.category === category.category && category.elements.some(element => {
+            return element.name === pair.start.name;
+          });
+        });
+        const validatedEnd = this.source.some(category => {
+          return pair.end.category === category.category && category.elements.some(element => {
+            return element.name === pair.end.name;
+          });
+        });
+        return validatedStart && validatedEnd;
+      });
+      this.noticeToParent();
+    }
+  },
+  watch: {
+    source: {
+      handler: function (val, oldVal) {
+        this.refreshPairs();
+      },
+      deep: true
     }
   }
 };
